52b4cbc88ab680dc5846e625f0cf7c26dfc84435
teuzinYTBR <matheus.diegues@outlook.com>
2021-12-20 14:37:45 -0300

    first commit

---
+++ jogo.js
@@ -0,0 +1,8 @@
+console.log('[TeuzinYTBR] Flappy Bird');
+console.log('Inscreva-se no canal :D https://www.youtube.com/TeuzinYTBR');
+
+const sprites = new Image();
+sprites.src = './img/sprites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
